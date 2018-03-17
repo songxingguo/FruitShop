cde2806705d1e8a30f1a30e0767098157f31a88d
songxingguo <1328989942@qq.com>
2018-03-17 16:33:39 +0800

    fruitShop

--- js/switchover.js
+++ js/switchover.js
@@ -4,6 +4,6 @@ $(".slider").YuxiSlider({
 	control:$('.control'), //绑定控制按钮
 	during:4000, //间隔4秒自动滑动
 	speed:800, //移动速度0.8秒
-	mousewheel:true, //是否开启鼠标滚轮控制
+	mousewheel:false, //是否开启鼠标滚轮控制
 	direkey:true //是否开启左右箭头方向控制
 });
